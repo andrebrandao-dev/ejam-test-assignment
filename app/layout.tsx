03de7a3a16e01b3b189e128709aae6bb2b4a9af9
André Brandão <andrebf4@gmail.com>
2023-09-23 19:04:47 -0300

    styles struct using sass

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,12 @@
 import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+import type { Metadata } from 'next'
+import { Manrope } from 'next/font/google'
+
+const manrope = Manrope({
+  subsets: ['latin'],
+  display: 'swap'
+})
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,7 +20,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={manrope.className}>{children}</body>
     </html>
   )
 }
