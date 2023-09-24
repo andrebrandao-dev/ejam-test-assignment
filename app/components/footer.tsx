7b85f930f0b94d548e9f9fca84b5c6e9d377605e
André Brandão <andrebf4@gmail.com>
2023-09-24 15:59:13 -0300

    styles footer

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,18 +1,21 @@
 import grid from '@/app/styles/modules/grid.module.scss'
+import footer from '@/app/styles/modules/footer.module.scss'
 
 export default function Header() {
   return (
-    <footer>
+    <footer className={footer.wrap}>
       <div className={grid.container}>
-        <div>
-          <span>Copyright (c) 2023</span>
-          <span className='divider'></span>
-          <a href='mailto:clarifionsupport@clarifion.com'>clarifionsupport@clarifion.com</a>
-        </div>
+        <div className={footer.content}>
+          <div className={footer.copy}>
+            <span>Copyright (c) 2023</span>
+            <span className={ footer.divider }></span>
+            <a className={footer.link} href='mailto:clarifionsupport@clarifion.com'>clarifionsupport@clarifion.com</a>
+          </div>
 
-        <div>
-           <span className='icon-lock'></span>
-           <span>Secure 256-Bit SSL Encryption.</span>
+          <div className={footer.secure}>
+            <span className='icon-lock'></span>
+            <span>Secure 256-Bit SSL Encryption.</span>
+          </div>
         </div>
       </div>
     </footer>  
